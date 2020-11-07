90d96a76f2c90c7828efe37382f9c63b87c8ecb4
RaulEfdz <raulfbth@criptext.com>
2020-11-07 11:36:15 -0500

    50% GALERIA

---
+++ GALERIA/Galeria_Seccion_2/Galeria_Seccion_2.js
@@ -0,0 +1,105 @@
+Fotos_Exteriores_H =[
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FExterior%2FHorizontales%2FIMG_0019.JPG?alt=media&token=b3f9106d-225d-481e-a21c-2d8d4bc2d02b',
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FExterior%2FHorizontales%2FIMG_0585.JPG?alt=media&token=4d47b384-6a55-4642-9603-8636d075426e',
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FExterior%2FHorizontales%2FIMG_3814.JPG?alt=media&token=c2017f3f-ffde-487a-bac4-a8171933b5cb'
+];
+Fotos_Exteriores_V =[
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FExterior%2FVerticales%2FIMG_0024.JPG?alt=media&token=dbdb6726-d2e6-47b9-9ff1-2c87b16b0e86',
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FExterior%2FVerticales%2FIMG_0303.JPG?alt=media&token=c696f0ca-59bf-4576-abaf-37739edc7018',
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FExterior%2FVerticales%2FIMG_0309.JPG?alt=media&token=f2b1ab86-f560-4008-a806-394768a73310',
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FExterior%2FVerticales%2FIMG_0371.JPG?alt=media&token=125d3090-ace4-465a-baa1-bf12b1817be6',
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FExterior%2FVerticales%2FIMG_3690.JPG?alt=media&token=cf22865a-56e6-4d86-b1a5-c4edffb71262',
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FExterior%2FVerticales%2FIMG_3841.JPG?alt=media&token=955f6a12-e16b-4e06-92b9-3108b8f96db6',
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FExterior%2FVerticales%2FIMG_9862.JPG?alt=media&token=00f4381a-3ffa-4bfc-aba9-2b0a0d17d986'
+];
+Fotos_Naturaleza_H =[
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FNaturaleza%2FHorizontal%2FIMG_6654..jpg?alt=media&token=f161a84f-a95e-472f-bfb3-7fa69f96f98e'
+];
+Fotos_Naturaleza_V =[
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FNaturaleza%2FVerticales%2FIMG_6654.JPG?alt=media&token=c70daaba-af1c-4f5d-b8a0-7f41df95f16d'
+];
+Fotos_Playa_H =[
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FPlaya%2FHorizontales%2FIMG_4134.JPG?alt=media&token=a96263ac-3fca-4422-9283-f2ade2028d3b'
+];
+Fotos_Playa_V =[
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FPlaya%2FVerticales%2FIMG_4185.JPG?alt=media&token=09f1fd87-e2a7-4169-97fa-e7fc63e1644a'
+];
+Fotos_Tradicionales_H =[
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FTradicionales%2FHorizontales%2FIMG_4164.JPG?alt=media&token=b30b7c1d-99a1-42b9-953d-84def848d5fa',
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FTradicionales%2FHorizontales%2FIMG_7693.JPG?alt=media&token=b3c7cda0-6b52-4cb0-bafb-2e25be9264e3'
+];
+Fotos_Tradicionales_v =[
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FTradicionales%2FVerticales%2FIMG_5124%20(1).JPG?alt=media&token=c3f38720-c5d8-4690-8cee-79720947dbba',
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FTradicionales%2FVerticales%2FIMG_5124.JPG?alt=media&token=4dea7cfe-a5f3-4d87-b468-95a3d6fc1e99',
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FTradicionales%2FVerticales%2FIMG_5154.JPG?alt=media&token=26966701-dd2b-40cc-a84c-34c518b0f358',
+    'https://firebasestorage.googleapis.com/v0/b/fotoseventosr-p.appspot.com/o/Fotos%2FTradicionales%2FVerticales%2FIMG_5202.JPG?alt=media&token=7471cad8-9dd2-4a40-b2c3-11b09b3ba66f'
+];
+
+N_Fotos_Exteriores_H = Fotos_Exteriores_H.length;
+N_Fotos_Exteriores_V = Fotos_Exteriores_H.length+Fotos_Exteriores_V.length;
+N_Fotos_Naturaleza_H = Fotos_Exteriores_H.length+Fotos_Exteriores_V.length+Fotos_Naturaleza_H.length;
+N_Fotos_Naturaleza_V  = Fotos_Exteriores_H.length+Fotos_Exteriores_V.length+Fotos_Naturaleza_H.length+Fotos_Naturaleza_V.length;
+N_Fotos_Playa_H  = Fotos_Exteriores_H.length+Fotos_Exteriores_V.length+Fotos_Naturaleza_H.length+Fotos_Naturaleza_V.length+Fotos_Playa_H.length;
+N_Fotos_Playa_V  = Fotos_Exteriores_H.length+Fotos_Exteriores_V.length+Fotos_Naturaleza_H.length+Fotos_Naturaleza_V.length+Fotos_Playa_H.length+Fotos_Playa_V.length;
+N_Fotos_Tradicionales_H  = Fotos_Exteriores_H.length+Fotos_Exteriores_V.length+Fotos_Naturaleza_H.length+Fotos_Naturaleza_V.length+Fotos_Playa_H.length+Fotos_Playa_V.length+Fotos_Tradicionales_H.length;
+N_Fotos_Tradicionales_v  = Fotos_Exteriores_H.length+Fotos_Exteriores_V.length+Fotos_Naturaleza_H.length+Fotos_Naturaleza_V.length+Fotos_Playa_H.length+Fotos_Playa_V.length+Fotos_Tradicionales_H.length+Fotos_Tradicionales_v.length;
+
+console.log('1 = '+N_Fotos_Exteriores_H);
+console.log('2 = '+N_Fotos_Exteriores_V);
+console.log('3 = '+N_Fotos_Naturaleza_H);
+console.log('4 = '+N_Fotos_Naturaleza_V);
+console.log('5 = '+N_Fotos_Playa_H);
+console.log('6 = '+N_Fotos_Playa_V);
+console.log('7 = '+N_Fotos_Tradicionales_H);
+console.log('8 = '+N_Fotos_Tradicionales_v);
+
+ 
+
+Fotos_Todas = [];
+Fotos_Todas = Fotos_Exteriores_H.concat(Fotos_Exteriores_V , Fotos_Naturaleza_H, Fotos_Naturaleza_V, Fotos_Playa_H, Fotos_Playa_V, Fotos_Tradicionales_H, Fotos_Tradicionales_v);
+
+Fot = []; 
+
+var Naturaleza_H_IA = [];
+for(i=0; Fotos_Todas.length>i; i++){
+document.getElementById("Contenedor_de_fotos").innerHTML +=`
+    
+    <div id="f">
+    
+        <img id="img_CdF${i}" class="img_CdF${i}" src="${Fotos_Todas[i]}"
+        onclick="i = ${i+1};
+        document.getElementById('svg_CdF${i}').style.color='rgb(255,20,20)';
+        Fot.push('${i}');
+        Calc();">
+
+        <svg id="svg_CdF${i}" viewBox="0 0 16 16" class="bi bi-bookmark-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M4 0a2 2 0 0 0-2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4zm4 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z"/>
+        </svg>
+
+    </div>
+ 
+    `;
+}
+
+// setTimeout(document.getElementById('img_CdF${i}').style.display='none', 2000)
+// document.getElementById('img_CdF${i}').style.animationPlayState
+
+var NATURALEZA = [];
+
+
+function Calc(){
+
+    for(var i = Fot.length -1; i >=0; i--){
+        if(Fot.indexOf(Fot[i]) !== i) Fot.splice(i,1);
+    }
+
+    for(var i=0; Fot.length>i; i++){
+        
+       if(Fot[i]>=0   && Fot[i]<= Number( N_Fotos_Exteriores_H)){
+            NATURALEZA = Number(NATURALEZA)+1;
+            console.log("Naturaleza"+NATURALEZA);
+       }
+
+    }
+}
+
