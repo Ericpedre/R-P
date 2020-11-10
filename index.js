5f232b1def386cd9d8ab8327f182132d357afb51
Eric Pedreschi <ericpedreschi@gmail.com>
2020-11-10 12:40:09 -0500

    implementacion de firebase hosting, functions y firestore

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const btnPaquetes = document.getElementById('btn-paquetes');
-btnPaquetes.addEventListener('click', () => {
-    console.log('flag')
-    firebase.analytics().logEvent('prueba', { name: 'testname'});
-})
+console.log('inicio')
+
 
 const btnInicio = document.getElementById('btn-inicio');
 btnInicio.addEventListener('click', () => {
     console.log('flag')
-    firebase.analytics().logEvent('Inicio', { name: 'click'});
+    firebase.analytics().logEvent('testabc', { name: 'tesabc'})
+    // firebase.analytics().logEvent('Inicio', { name: 'click'});
+    console.log("flag2")
 })
+
+console.log('fin')
